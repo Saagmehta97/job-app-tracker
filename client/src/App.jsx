ea5caa16545cc1c17f8e79b602f8347e2b930842
DanHudgens <danw.hudgens@gmail.com>
2024-06-15 09:56:19 -0700

    add login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Signup from './components/signup.jsx'
 
 export default function App() {
   return (
+    <div>
     <div>hello</div>
+    <Signup />
+    </div>
+    
   )
   
 }
