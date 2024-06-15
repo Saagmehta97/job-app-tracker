74dc6a84fff5ce89a4b4b107344812449d3733fd
DanHudgens <danw.hudgens@gmail.com>
2024-06-15 14:21:32 -0700

    add verifyUser middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,26 +1,28 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const db = require('../models/userModels')
-
+const db = require('../models/userModels');
 
 const userController = {};
 
 userController.hashing = async (req, res, next) => {
-  const { password } = req.body;    
-try {
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hashWord = await bcrypt.hash(password, salt);
-  res.locals.hashWord = hashWord;
-  return next();
-} catch(err) {
-  return next({log: err, message: {err: "BIG error in userController.hashing"}});
-}
+  const { password } = req.body;
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashWord = await bcrypt.hash(password, salt);
+    res.locals.hashWord = hashWord;
+    return next();
+  } catch (err) {
+    return next({
+      log: err,
+      message: { err: 'BIG error in userController.hashing' },
+    });
+  }
 };
 
 userController.createUser = async (req, res, next) => {
-  const { firstName, lastName, username } = req.body;    
+  const { firstName, lastName, username } = req.body;
   try {
-    const hashWord = res.locals.hashWord
+    const hashWord = res.locals.hashWord;
     // console.log('firstName is: ', firstName);
     // console.log('ln is: ', lastName);
     // console.log('user is: ', username);
@@ -30,15 +32,36 @@ userController.createUser = async (req, res, next) => {
       lastName: lastName,
       username: username,
       password: hashWord,
-    }
+    };
     res.locals.newUser = newUser;
     return next();
-  } catch(error) {
-    return next({log: err, message: {err: "small error in userController.createUser"}})
+  } catch (error) {
+    return next({
+      log: err,
+      message: { err: 'small error in userController.createUser' },
+    });
+  }
+};
+
+userController.verifyUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const passQuery = `SELECT password FROM users WHERE username = ${username}`;
+  //query for the stored password in the database using the username as the condition
+  //use bcrypt compare to check entered password and stored password
+  try {
+    const storedPass = await db.query(passQuery);
+    const queryResult = await bcrypt.compare(password, storedPass);
+    if (queryResult) {
+      res.locals.password = true;
+    }
+    return next();
+  } catch (err) {
+    return next({
+      log: err,
+      message: { err: 'small error in userController.verifyUser' },
+    });
   }
 };
 
 module.exports = userController;
-
-
-
--- server/routes/users.js
+++ server/routes/users.js
@@ -8,17 +8,26 @@ const routerSignup = express.Router();
 // ADD STARTER DATA REQUEST ROUTE HANDLER HERE
 
 routerSignup.get('/', (req, res) => {
-  res.json({message: "Server Workin"})
+  res.json({ message: 'Server Workin' });
 });
 
-routerSignup.post('/signup', userController.hashing, userController.createUser, (req, res) => {
-    console.log(req.body)
+routerSignup.post(
+  '/signup',
+  userController.hashing,
+  userController.createUser,
+  (req, res) => {
+    console.log(req.body);
     return res.status(200).json(res.locals.newUser);
-})
+  }
+);
 
-
+routerSignup.post('/login', userController.verifyUser, (req, res) => {
+  return res.status(200).json(res.locals.password);
+});
 // ADD GET MORE CHARACTERS ROUTE HANDLER HERE
 
-
-
 module.exports = routerSignup;
+//amcatee
+//codesmith
+//ashe
+//mcatee
